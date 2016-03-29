4c00d60618b97145aade0fe841357deef25d6add
A39120 <lariachi@gmail.com>
2016-03-29 16:00:20 +0100

    first commit

---
+++ src/main/java/util/FileGetter.java
@@ -0,0 +1,53 @@
+package util;
+
+import wthr.model.WeatherInfo;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/**
+ * Created by João on 24/03/2016.
+ */
+public class FileGetter {
+
+    String filePath;
+
+    public static <T> List<T> FileGet(String filePath, Function<BufferedReader, List<T>> conv) throws IOException, URISyntaxException {
+        /*Path p = Paths.get(ClassLoader.getSystemResource(filePath).toURI());
+        Iterator<String> lines = Files.readAllLines(p).iterator();
+        while(lines.next().startsWith("#"));
+        return conv.apply(lines, new );*/
+        try (InputStream in = new FileInputStream(filePath)) {
+            try(BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+                String line;
+                while ((line = reader.readLine()).startsWith("#"));
+                return conv.apply(reader);
+            }
+        }
+    }
+
+    public static File[] listPath(String path){
+        File folder = new File(path);
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                System.out.println("File " + listOfFiles[i].getName());
+            } else if (listOfFiles[i].isDirectory()) {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }
+        }
+
+        return listOfFiles;
+    }
+}
---
+++ src/main/java/wthr/WeatherFileGetterFromCsv.java
@@ -0,0 +1,15 @@
+package wthr;
+
+/**
+ * Created by João on 24/03/2016.
+ */
+public class WeatherFileGetterFromCsv {
+    private final String RESOURCE_PATH = "src/main/resources/data/";
+
+    public String containsFile(String name, int population){
+
+    }
+
+
+
+}
